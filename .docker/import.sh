500102239fe18c957754906280992ec8b15e3653
Dan Webb <dan.webb@damacus.io>
2017-10-21 23:08:46 +0100

    Use the new saner script

--- .docker/import.sh
+++ .docker/import.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-NAME="docker-builder"
-export PROJECT="${NAME:-$CIRCLE_PROJECT_REPONAME}"
+export PROJECT="docker-builder"
 export DESCRIPTION="Docker container with Docker, Inspec and Ruby"
 export MAINTAINER="damacus"
 
 if ! [[ -e .docker/functions.sh ]];then
-  wget -O .docker/functions.sh "https://raw.githubusercontent.com/damacus/docker-build-scripts/master/functions.sh"
+  wget -O .docker/functions.sh "https://raw.githubusercontent.com/damacus/docker-build-scripts/remove_deps/functions.sh"
 fi
 
 source ".docker/functions.sh"
