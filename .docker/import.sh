7fee37a6507959fa2be5d879c45a85c4b82c8250
Dan Webb <dan.webb@damacus.io>
2017-10-22 16:33:20 +0100

    Switch mack to master

--- .docker/import.sh
+++ .docker/import.sh
@@ -4,7 +4,7 @@ export DESCRIPTION="Docker container with Docker, Inspec and Ruby"
 export MAINTAINER="damacus"
 
 if ! [[ -e .docker/functions.sh ]];then
-  wget -O .docker/functions.sh "https://raw.githubusercontent.com/damacus/docker-build-scripts/remove_deps/functions.sh"
+  wget -O .docker/functions.sh "https://raw.githubusercontent.com/damacus/docker-build-scripts/master/functions.sh"
 fi
 
 source ".docker/functions.sh"
